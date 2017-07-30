c79c95b164030da113d424b6d18474c70f8c835d
mehulagarwal <mehul96mail@gmail.com>
2017-07-31 05:17:49 +0530

    Added a review Page.

--- launch/views.py
+++ launch/views.py
@@ -5,6 +5,7 @@ from Film.review_info1 import get_info
 from Film.check_and_add import check_movie
 from Film.mov import get_review
 # from Film.get_poster import poster
+import sys
 
 
 @app.route('/top')
@@ -26,11 +27,12 @@ def search():
     if request.method == 'POST':
         # Get data from form
         # Call Script review_info1.py
+        # session.clear()
         movie = request.form['title']
         params = get_info(movie)
-
+        review = get_review(params[0])
         # print(review)
-        print(type(params))
+        # print(type(params))
         # params.append(review)
         # More stuff
         print('Checking movie in database, wait')
@@ -38,12 +40,17 @@ def search():
         session.pop('params', None)
         print('no params')
         session['params'] = params
+        # session.modified = True
         movie = params[0]
         year = params[1]
         print(str(movie) + ', ' + str(year))
         # link = poster(movie, year)
-        # session['review'] = review
+        if review is not None:
+            session['review'] = review[:511] + '...'
+        session.modified = True
+        # print('search : ' + str(session.modified))
         # print(session['review'])
+        # print(session)
         return redirect(url_for('result'))
 
     return render_template('search.html', error=error)
@@ -52,22 +59,37 @@ def search():
 @app.route('/movie/<name>')
 def show_movie(name):
     params = get_info(name)
-    check_movie(name=params[0], year=params[1])
-    session.pop('params', None)
+    # session.clear()
+    # check_movie(name=params[0], year=params[1])
+    # session.pop('params', None)
     session['params'] = params
     session.modified = True
+    # print(session['params'])
     movie = params[0]
     year = params[1]
     print(str(movie) + ', ' + str(year))
+    review = get_review(name)
+    if review is not None:
+        session['review'] = review[:511] + '...'
+    session.modified = True
+    # print('show_movie : ' + str(session.modified))
+    # print(session)
     return redirect(url_for('result'))
 
 
 @app.route('/result')
 def result():
+    # print(request.cookies)
+    # print(session)
     params = session['params']
-    # review = session['review']
+
+    if 'review' in session:
+        review = session['review'][:511] + '....'
+    else:
+        review = 'No summary available.'
     # return params[0]
-    return render_template('movie.html', params=params)
+    # print(review)
+    return render_template('movie.html', params=params, review=review)
 
 
 @app.route('/genre', methods=['GET', 'POST'])
@@ -89,4 +111,19 @@ def genre_search():
                                  (genre,))
         return render_template('view_genre.html', genre=genre, m_list=m_list)
     return render_template('get_genre.html', error=error)
+
+
+@app.route('/review')
+def show_review():
+
+    try:
+        params = session['params']
+    except:
+        print(sys.exc_info()[0])
+        # info = ['None', 'None']
+    review = get_review (params[0])
+    if review is None:
+        review = 'No review available at the moment, sorry.'
+    return render_template('show_review.html', review=review, params=params)
+
 
